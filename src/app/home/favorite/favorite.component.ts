da507b3c1d5e3fe842dd4a431934703e39ad92a0
Gabriel <gabrielmavilela@gmail.com>
2022-08-08 11:49:36 -0300

    commit 2

--- src/app/home/favorite/favorite.component.ts
+++ src/app/home/favorite/favorite.component.ts
@@ -10,6 +10,16 @@ import { Router } from '@angular/router';
 })
 export class FavoriteComponent implements OnInit {
 
+  pegarDiferenca() {
+    let r1 = [2,4,6,8];
+    let r2 = [3,4,5,7,9];       
+    let r3 = r1.filter( a => !r2.includes( a ) );
+
+    console.log( r3 );
+}
+
+
+
   catData: any[] | undefined
 
   constructor(private catService: catApiService, private router: Router) { }
@@ -24,6 +34,7 @@ export class FavoriteComponent implements OnInit {
     this.catService.pullFavCats().subscribe((fav)=>{
       this.catData = fav
       console.log(fav)
+      this.pegarDiferenca();
     })
   }
 
